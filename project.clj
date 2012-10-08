22fcf05714b9df9de06f9b2f1f477b835db287de
Ville Immonen <ville.immonen@iki.fi>
2012-10-08 06:48:43 +0300

    Cleaned up dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljs-fiddle "0.0.1"
-  :description "A simple example of how to use lein-cljsbuild"
+  :description "A simple web application for trying out ClojureScript"
   ; Source path for Leiningen 1.x:
   :source-path "src/clj"
   ; Source paths for Leiningen 2.x:
@@ -10,14 +10,10 @@
                  [tentacles "0.2.2"]
                  [jayq "0.1.0-alpha3"]
                  [crate "0.2.1"]]
-  :dev-dependencies [[lein-ring "0.7.0"]]
-  :plugins [[lein-cljsbuild "0.2.7"]
-            [lein-ring "0.7.5"]]
+  :plugins [[lein-cljsbuild "0.2.7"]]
   :cljsbuild {
     :builds [{:source-path "src/cljs"
               :compiler {:output-to "resources/public/js/main.js"
                          :optimizations :simple
                          :pretty-print true}}]}
-  :ring {:handler fiddle.routes/app
-         :auto-refresh? true}
   :main fiddle.server)
